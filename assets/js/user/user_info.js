4fcf63ed5baaaf0446f0acc34e5bebb81964e42a
lvwenqin <2466791373@qq.com>
2022-04-29 21:34:38 +0800

    完成个人中心功能的开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,57 @@
+$(function(){
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        nickname:function(value){
+            if(value.length>6){
+                return '昵称长度必须在1~6字符'
+            }
+        }
+    })
+
+initUserInfo()
+
+//初始化用户的基本信息
+function initUserInfo(){
+    $.ajax({
+        method:'GET',
+        url:'/my/userinfo',
+        success:function(res){
+            if(res.status !==0){
+                return layer.msg('获取用户信息失败！')
+            }
+            console.log(res)
+            
+            form.val('formUserInfo', res.data)
+        }
+    })
+}
+
+
+//重置表单的数据
+$('#btnReset').on('click',function(e){
+    //阻止表单的默认行为
+    e.preventDefault()
+    initUserInfo()
+})
+
+
+//提交表单数据
+$('.layui-form').on('submit',function(e){
+    //阻止表单的默认行为
+    e.preventDefault()
+    //发起ajax数据请求
+    $.ajax({
+        method:'POST',
+        url: '/my/userinfo',
+        data:$(this).serialize(),
+        success:function(res){
+            if(res.status !==0){
+                return layer.msg('更新用户信息失败')
+            }
+            layer.msg('更新用户信息成功！')
+            window.parent.getUserInfo()
+        }
+    })
+})
+})
